a8e523c9bd11170ba95c5b366acc1ba57eb4039f
River Wallerstedt <riverwallerstedt@gmail.com>
2025-10-01 10:36:12 -0500

    Update queue.py
    Just implemented a basic Queue. Hopefully we don't need much more than this in this file. It's probably going to take a little to implement it in main with the other structures however.

---
+++ OperationsControlSim/Structures/queue.py
@@ -0,0 +1,24 @@
+'''
+River Wallerstedt:
+Simple Queue to manage events for the Operations Control Simulation
+Implements a basic FIFO queue structure
+'''
+class Queue:
+    def __init__(self):
+        self.events = []
+
+    def enqueue(self, event):
+        self.events.append(event)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.events.pop(0)
+        return None
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.events[0]
+        return None
+    
+    def is_empty(self):
+        return len(self.events) == 0
